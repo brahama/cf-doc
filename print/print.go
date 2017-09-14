3906512e7389f06bb8f97e03ff4369a85aa8b007
Brahama <agus.262@gmail.com>
2017-09-14 02:22:14 -0300

    Add Pretty Print

--- print/print.go
+++ print/print.go
@@ -1,1 +1,11 @@
 package print
+
+import "github.com/brahama/cf-doc/doc"
+import "encoding/json"
+
+//Pretty prints pretty json of Doc
+func Pretty(d *doc.Doc) (string, error) {
+	j, err := json.MarshalIndent(d, "", "    ")
+
+	return string(j), err
+}
