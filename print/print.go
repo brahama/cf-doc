1dde961254761cefc7835f228476355fcb174758
Brahama <agus.262@gmail.com>
2017-09-14 02:58:14 -0300

    Add Print Markdown Parameters

--- print/print.go
+++ print/print.go
@@ -1,11 +1,44 @@
 package print
 
-import "github.com/brahama/cf-doc/doc"
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/brahama/cf-doc/doc"
+)
 
 //Pretty prints pretty json of Doc
 func Pretty(d *doc.Doc) (string, error) {
 	j, err := json.MarshalIndent(d, "", "    ")
 
 	return string(j), err
+}
+
+// Markdown prints the doc as Markdown
+func Markdown(d *doc.Doc) (string, error) {
+	var buf bytes.Buffer
+
+	if len(d.Usage) > 0 {
+		buf.WriteString(fmt.Sprintf("%s\n", d.Usage))
+	}
+
+	if len(d.Parameters) > 0 {
+		buf.WriteString("\n## Parameters\n\n")
+		buf.WriteString("| Name | Description | Type | Default | Allowed Values |\n")
+		buf.WriteString("|------|-------------|:----:|:-------:|:---------------|\n")
+
+		for k := range d.Parameters {
+			buf.WriteString(fmt.Sprintf("| %s | %s | %s | %s | %s |\n",
+				d.Parameters[k].Name,
+				d.Parameters[k].Description,
+				d.Parameters[k].Type,
+				d.Parameters[k].Default,
+				d.Parameters[k].AllowedValues))
+		}
+
+	}
+
+	return buf.String(), nil
+
 }
